2d02551383e8d7cf1e4db30c7a150c27df2d1c48
Anuj Parakh <anuj@anujinfotech.com>
2020-09-13 01:31:09 -0500

    Changed to server-client model, removed current iOS app. Working server for bulbs with node.js and POST requests

---
+++ Server/bulbTalker.js
@@ -0,0 +1,70 @@
+const noble = require('@abandonware/noble');
+const config = require('./config.json')
+let bulbPeripherals = {}
+let bulbNames = []
+
+noble.on('stateChange', async (state) => {
+    if (state === 'poweredOn') {
+        await noble.startScanningAsync([], false);
+    }
+});
+
+noble.on('discover', async (peripheral) => {
+    if (peripheral.advertisement.localName == null)
+        return
+    let peripheralName = peripheral.advertisement.localName
+    if (peripheralName.includes('Triones')) {
+        // Found new bulb!
+        if (!bulbNames.includes(peripheralName)) {
+            console.log('Discovered: ' + peripheralName)
+            bulbPeripherals[peripheralName] = {}
+            bulbPeripherals[peripheralName]['peripheral'] = peripheral
+            bulbNames.push(peripheralName)
+        }
+        // Found enough bulbs
+        // TODO: Convert this into a timeout
+        if (bulbNames.length >= config.numBulbs) {
+            await noble.stopScanningAsync()
+            await connectToBulbs()
+        }
+    }
+});
+
+async function connectToBulbs() {
+    console.log('\n*** Connecting to Bulbs ***\n')
+    for (bulbName of bulbNames) {
+        await bulbPeripherals[bulbName].peripheral.connectAsync()
+        console.log(bulbName + ' Connected!')
+        try {
+            const services = await bulbPeripherals[bulbName].peripheral.discoverServicesAsync([config.identifiers.writeServiceUUID]);
+            const characteristics = await services[0].discoverCharacteristicsAsync()
+            bulbPeripherals[bulbName].writeCharacteristic = characteristics[0]
+            console.log('Saved Characteristic!')
+        } catch (e) {
+            // handle error
+            console.log(e)
+        }
+    }
+}
+
+async function isReady() {
+    numConnected = 0
+    for (bulbName of bulbNames) {
+        if (bulbPeripherals[bulbName].writeCharacteristic != null)
+        {
+            ++numConnected
+        }
+    }
+    console.log('Num: ' + numConnected)
+    return (numConnected == config.numBulbs)
+}
+
+module.exports.isReady = isReady
+
+module.exports.sendBulbMessage = async (hexMessage) => {
+
+    let toWrite = Buffer.from(hexMessage, 'hex')
+    for (bulbName of bulbNames) {
+        bulbPeripherals[bulbName].writeCharacteristic.writeAsync(toWrite, false)
+    }
+}
---
+++ Server/index.js
@@ -0,0 +1,29 @@
+const bulbTalker = require('./bulbTalker')
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+const port = 3000
+
+app.get('/', async (req, res) => {
+})
+
+app.post('/command', async(req, res) => {
+    let command = req.body.command
+    if(await bulbTalker.isReady())
+    {
+        console.log('Command is: ' + command)
+        await bulbTalker.sendBulbMessage(command)
+        return res.send('Command Sent')
+    }
+    return res.send('Bulbs Not Ready')
+
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
